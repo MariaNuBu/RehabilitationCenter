bfe4df729b237f40dac3b3cff96f30b18daba9ef
msmar <msmar@DESKTOP-EKOM3T3>
2020-03-30 12:51:02 +0200

    30/03/2020

--- RehabilitationCenter/src/db/sqlite/SQLitePatientManager.java
+++ RehabilitationCenter/src/db/sqlite/SQLitePatientManager.java
@@ -1,15 +1,46 @@
 package db.sqlite;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import db.interfaces.PatientManager;
-import pojos.Appointment;
-import pojos.Patient;
+import pojos.*;
+
 
 public class SQLitePatientManager implements PatientManager {
 
+	private Connection c;
+	
+	public SQLitePatientManager(Connection c) {
+		this.c = c;
+	}
+	
 	@Override
 	public Patient searchPatient(Integer ID) {
-		// TODO Auto-generated method stub
-		return null;
+		Patient p = null;
+		try 
+		{
+			String sql="SELECT * FROM patients WHERE ID LIKE ?";
+			PreparedStatement prep=c.prepareStatement(sql);
+			prep.setInt(1, ID);
+			ResultSet rs=prep.executeQuery();
+			int id=rs.getInt("ID");
+			String name=rs.getString("Name");
+			String adress=rs.getString("Adress");
+			Date DOB=rs.getDate("DOB");
+			Integer phone = rs.getInt("Phone");
+			String email=rs.getString("Email");
+			String sportType=rs.getString("SportType");
+			String disability=rs.getString("Disability");
+			p = new Patient(id,name,adress,DOB,phone,email,sportType,disability);
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return p;
 	}
 
 	@Override
