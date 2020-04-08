fe8c36077d69cb434214493dc91904723f051755
Cristina <Cristina@LAPTOP-4PQ48R5S>
2020-04-08 19:34:29 +0200

    parte cristi ole

--- RehabilitationCenter/src/db/sqlite/SQLiteDoctorManager.java
+++ RehabilitationCenter/src/db/sqlite/SQLiteDoctorManager.java
@@ -21,7 +21,7 @@ public class SQLiteDoctorManager implements DoctorManager {
 	}
 	@Override
 	public void readMH(Integer ID) {
-		MedicalHistory m = null;
+		MedicalHistory mh = null;
 		try {
 			String sql = "SELECT * FROM medicalhistory WHERE ID=?";
 			PreparedStatement ps = c.prepareStatement(sql);
@@ -38,7 +38,7 @@ public class SQLiteDoctorManager implements DoctorManager {
 				Float weightKg = rs.getFloat("weightKg");
 				Integer heightCm = rs.getInt("heightCm");
 						
-				m = new MedicalHistory(id,name,DOB,desease,allergies,surgeries,weightKg,heightCm);
+				mh = new MedicalHistory(id,name,DOB,desease,allergies,surgeries,weightKg,heightCm);
 			}
 			rs.close();
 			ps.close();
