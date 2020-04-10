083e3f09335dc4851937ec3c2e30de11a508d5d6
MariaNuBu <mi.nunez1@usp.ceu.es>
2020-04-10 18:05:55 +0200

    fuck gui

--- RehabilitationCenter/src/gui/Main.java
+++ RehabilitationCenter/src/gui/Main.java
@@ -1,31 +1,63 @@
 package gui;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import db.interfaces.DBManager;
+import db.interfaces.PatientManager;
+import db.interfaces.PhysicalTherapistManager;
+import db.sqlite.SQLiteManager;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import pojos.Patient;
+import ui.PatientMenu;
 
 public class Main extends Application 
 {
-	Scene scene1,scene2;
+	private static DBManager db;
+	private static PatientManager pm;
+	private static PhysicalTherapistManager ptm;
 	@Override
 	public void start(Stage window) throws Exception 
 	{
-		window.setTitle("primera aplicacion");
-		Button b=new Button("Go to scene 2");
-		b.setOnAction(e->window.setScene(scene2));
-		StackPane layout=new StackPane();
-		layout.getChildren().add(b);
-		Scene scene1 = new Scene(layout,400,550);
-		StackPane layout2=new StackPane();
-		Button b2=new Button("Back to 1");
-		b2.setOnAction(e->window.setScene(scene1));
-		layout2.getChildren().add(b2);
-		scene2=new Scene(layout2,600,300);
-		window.setScene(scene1);
+		db= new SQLiteManager();
+		db.connect();
+		db.createTables();
+		pm = db.getPatient();
+		ptm=db.getPhysicalTherapist();
+		window.setTitle("Paralimpic's Rehabilitation Center");
+		Button patient=new Button("Patient");
+		Button doctor=new Button("Doctor");
+		Button physicalTherapist= new Button("PhysicalTherapist");		
+		VBox layout=new VBox();
+		layout.setPadding(new Insets(20,20,20,20));
+		patient.setOnAction(e-> 
+		{
+			try {
+				PatientMenu.registerPatient();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			window.close();
+		});
+		layout.getChildren().addAll(patient,doctor,physicalTherapist);
+		layout.setAlignment(Pos.CENTER);
+		Scene scene = new  Scene(layout,100,300);
+		window.setScene(scene);
 		window.show();
 	}
+	
+	
+
 	public static void main (String [] args)
 	{
 		launch(args);
