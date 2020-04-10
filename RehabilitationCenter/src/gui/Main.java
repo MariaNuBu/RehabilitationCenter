195bb4ff3e9c6dc6f8b88002ac9a4da6f7e1d86f
Teresa Romero <Teresa Romero@DESKTOP-D86NHTN>
2020-04-10 17:42:28 +0200

    .

--- RehabilitationCenter/src/gui/Main.java
+++ RehabilitationCenter/src/gui/Main.java
@@ -1,10 +1,12 @@
 package gui;
 
 import javafx.application.Application;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 public class Main extends Application 
 {
