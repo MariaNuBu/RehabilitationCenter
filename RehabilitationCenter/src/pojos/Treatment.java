620bf6bb91cfa56bc1418232a99e33408ad2cf3b
Teresa Romero <Teresa Romero@DESKTOP-D86NHTN>
2020-05-20 20:11:13 +0200

    Aqui ha habido un conflicto pero no he cambiado nada

--- RehabilitationCenter/src/pojos/Treatment.java
+++ RehabilitationCenter/src/pojos/Treatment.java
@@ -10,8 +10,8 @@ public class Treatment  implements Serializable  {
 	private static final long serialVersionUID = 1L;
     private Integer id ;
     private String type ;
-    private Integer lenght ; 
-    
+    private Integer lenght ;
+
     public Treatment() {
     	super();
     }
@@ -19,7 +19,7 @@ public class Treatment  implements Serializable  {
 	  this.id=id ;
 	  this.type=type;
 	  this.lenght=lenght;
-	  
+
   }
   public Treatment(String type, Integer lenght) {
 	  this.type = type;
@@ -43,41 +43,43 @@ public Integer getLenght() {
 public void setLenght(Integer lenght) {
 	this.lenght = lenght;
 }
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((id == null) ? 0 : id.hashCode());
-	result = prime * result + ((lenght == null) ? 0 : lenght.hashCode());
-	result = prime * result + ((type == null) ? 0 : type.hashCode());
-	return result;
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lenght == null) ? 0 : lenght.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Treatment other = (Treatment) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lenght == null) {
+			if (other.lenght != null)
+				return false;
+		} else if (!lenght.equals(other.lenght))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
 		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Treatment other = (Treatment) obj;
-	if (id == null) {
-		if (other.id != null)
-			return false;
-	} else if (!id.equals(other.id))
-		return false;
-	if (lenght == null) {
-		if (other.lenght != null)
-			return false;
-	} else if (!lenght.equals(other.lenght))
-		return false;
-	if (type == null) {
-		if (other.type != null)
-			return false;
-	} else if (!type.equals(other.type))
-		return false;
-	return true;
-}
-  
-  
-}
+	}
+
+
+
+
+	}
